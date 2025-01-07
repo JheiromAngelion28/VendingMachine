c95770f721b7ab478180fae44efd7c2adc836b54
Jheirom <jheirom.amp@gmail.com>
2025-01-07 13:45:38 +0400

    Create Vending Machine version 1.py

---
+++ Vending Machine version 1.py
@@ -0,0 +1,49 @@
+money = 100.00
+
+vending_machine = {
+    "Chips Spicy FLavor ":"0A",
+    "Chips Onion Flavor":"0B",
+    "Chips Tomato Flavor ":"0C",
+    "Chips Brisket Flavor":"0D",
+    "Instant Noodles Chicken Flavor":"0E",
+    "Instant Noodles Beef Flavor":"0F",
+    "Instant Noodles Shrimp Flavor":"0G",
+    "Cocacola Soda":"1A",
+    "Pepsi Soda":"1B",
+    "Diet Cock Soda":"1C",
+    "Chocolate Milk Shake":"1D",
+    "Strawberry Milk Shake":"1E",
+    "Iced Tea":"1F",
+    "gatorade Energy Drink ":"1G",
+    "Evian Water":"2A",
+    "Arwa Water":"2B",
+    "Masafi ":"2C",
+    "Figi Water":"2D",
+    "Boba Milk Tea Caramel Flavore ":"2E",
+    "Boba Milk Chocolate Milk Flavor":"2F",
+    "Boba Milk Cheese Flavor":"2G"
+}
+
+prices = {
+    "Chips Spicy FLavor ":"15AED",
+    "Chips Onion Flavor":"15AED",
+    "Chips Tomato Flavor ":"15AED",
+    "Chips Brisket Flavor":"15AED",
+    "Instant Noodles Chicken Flavor":"10AED",
+    "Instant Noodles Beef Flavor":"10AED",
+    "Instant Noodles Shrimp Flavor":"10AED",
+    "Cocacola Soda":"5AED",
+    "Pepsi Soda":"5AED",
+    "Diet Cock Soda":"5AED",
+    "Chocolate Milk Shake":"10AED",
+    "Strawberry Milk Shake":"10AED",
+    "Iced Tea":"10AED",
+    "gatorade Energy Drink ":"10AED",
+    "Evian Water":"10AED",
+    "Arwa Water":"1AED",
+    "Masafi ":"1AED",
+    "Figi Water":"10AED",
+    "Boba Milk Tea Caramel Flavor ":"22AED",
+    "Boba Milk Chocolate Milk Flavor":"22AED",
+    "Boba Milk Cheese Flavor":"22AED"
+}
